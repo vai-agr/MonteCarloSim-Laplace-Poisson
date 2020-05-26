76a7bce4a15cc8080024894b81ed1168a6482537
Vaibhav <vaiagr2011@gmail.com>
2020-05-26 21:04:03 +0530

    Added probability measurement for each walk in random_walk.py

--- laplace_solution.py
+++ laplace_solution.py
@@ -3,5 +3,13 @@ import random_walk as rw
 
 if __name__ == "__main__":
 
-	ans = rw.randomWalk1D(10,5,10,[0,10],1)
-	print(ans)
+	P_a =[]
+	for i in range(1,10,1):
+
+		(ans,prob) = rw.randomWalk1D(10,i,10,[0,10],1)
+		#print(ans)
+		P_a.append(prob)
+	print(P_a)	
+		
+
+
--- random_walk.py
+++ random_walk.py
@@ -4,13 +4,17 @@ import random
 
 def randomWalk1D(number_of_walks, starting_point, length, boundary_condition, step_size=1):
 	path = []
-	
+	probability = 0
+
 	for i in range(number_of_walks):
+
 		curr_point = starting_point
 		curr_path = []
 		while(True):
 			d = random.randint(0,1)
 			if curr_point in boundary_condition:
+				probability = probability+ (curr_point/boundary_condition[1])
+				#print(probability)
 				break 
 			if d>=0.5:
 				curr_point = min(curr_point+step_size, boundary_condition[1])
@@ -19,7 +23,9 @@ def randomWalk1D(number_of_walks, starting_point, length, boundary_condition, st
 			curr_path.append(curr_point)
 		path.append(curr_path)
 
-	return path
+		
+	probability = probability/number_of_walks	
+	return (path,probability)
 
 
 
