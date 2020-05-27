fc632e02f30aa57228b30d1e35e8cab82981c03e
Vaibhav <vaiagr2011@gmail.com>
2020-05-27 16:00:06 +0530

    Created poisson_solution.py and Added 'f'.

--- poisson_solution.py
+++ poisson_solution.py
@@ -3,20 +3,22 @@
 import random_walk as rw
 import matplotlib.pyplot as plt
 
-number_of_walks = 400		 #number of random walks for each point
-step_size = 1				 #distance between each point 
+number_of_walks = 400		 #Number of random walks for each point
+step_size = 1				 #Distance between each point 
 length = 10					 #Distance between the plates of the capacitor in cm	
 boundary_voltages = [0,5]    #Voltages of the two plates of the capacitor in Volts
-
+epsilon_o = 8.85e-12		 #Permittivity of free space
+rho = 1e-14		 			 #Charge density in C/m^3	
 
 if __name__ == "__main__":
 
-	P_a =[]
+	f = -rho/epsilon_o
+	P_a = []
 	for point in range(0,length,step_size):
-
 		(ans,prob) = rw.randomWalk1D(number_of_walks,point,[0,length],step_size)  #length starts at 0
 		#print(ans)
 		P_a.append(prob)
+
 	print(P_a)
 	voltage_final = [(i*boundary_voltages[1]+ (1-i)*boundary_voltages[0]) for i in P_a]
 	plt.plot(voltage_final)
