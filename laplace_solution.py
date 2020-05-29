e9c40d0253871e5366784a88524e544b874b0e51
Vaibhav <vaiagr2011@gmail.com>
2020-05-29 22:08:47 +0530

    Changed the parameters of random walk and stats of capacitor, modified the graph to add more detail

--- laplace_solution.py
+++ laplace_solution.py
@@ -1,12 +1,12 @@
-import random_walk as rw
-import matplotlib.pyplot as plt
+import random_walk as rw       #imported random_walk.py for using the randomWalk1D function
+import matplotlib.pyplot as plt   #for graphing functions
 
-number_of_walks = 100		 #Number of random walks for each point
+number_of_walks = 200		 #Number of random walks for each point
 step_size = 1				 #Distance between each point = 1cm ie 10 divisions
-length = 10					 #Distance between the plates of the capacitor = 10 cm	
-boundary_voltages = [0,5]    #Voltages of the two plates of the capacitor in Volts
+length = 20					 #Distance between the plates of the capacitor = 10 cm	
+boundary_voltages = [-10,10]    #Voltages of the two plates of the capacitor in Volts
 
-if __name__ == "__main__":
+if __name__ == "__main__":		#the main part
 
 	P_a =[]
 	points = [i for i in range(0,length+step_size,step_size)]
@@ -14,10 +14,17 @@ if __name__ == "__main__":
 
 		(ans,prob) = rw.randomWalk1D(number_of_walks,point,[0,length],step_size)  #length starts at 0
 		#print(ans)
-		P_a.append(prob)
+		P_a.append(prob)				#Probability of reaching boundary(whilst Random Walking) for all the points
+
 	print(P_a)
 	voltage_final = [(i*boundary_voltages[1]+ (1-i)*boundary_voltages[0]) for i in P_a]
 	voltage_actual = [boundary_voltages[0]+(i/length)*(boundary_voltages[1]- boundary_voltages[0]) for i in points]
-	plt.plot(voltage_final, marker='o')
-	plt.plot(voltage_actual)
+
+	p1 = plt.plot(voltage_final, marker='o', markersize=4)
+	p2 = plt.plot(voltage_actual)
+	plt.title("LAPLACE EQUATION")
+	plt.xlabel("Distance from the low voltage capacitor plate (in cm)")
+	plt.ylabel("Voltage (in Volts)")
+	plt.xticks(range(0,length+step_size,2*step_size))
+	plt.legend((p1[0], p2[0]),('Calculated Voltage', 'Actual Voltage'))
 	plt.show()
--- poisson_solution.py
+++ poisson_solution.py
@@ -1,12 +1,13 @@
 import random_walk as rw     #imported random_walk.py for using the randomWalk1D function
 import matplotlib.pyplot as plt  #for graphing functions
 
-number_of_walks = 400		 #Number of random walks for each point
+number_of_walks = 200		 #Number of random walks for each point
 step_size = 1				 #Distance between each point = 1cm i.e. 10 divisions
-length = 10					 #Distance between the plates of the capacitor= 10cm	
-boundary_voltages = [0,5]    #Voltages of the two plates of the capacitor in Volts
+length = 20					 #Distance between the plates of the capacitor= 10cm	
+boundary_voltages = [-10,10]    #Voltages of the two plates of the capacitor in Volts
 epsilon_o = 8.85e-12		 #Permittivity of free space
-rho = 1e-14		 			 #Charge density in C/m^3	
+rho = 1e-13		 			 #Charge density in C/m^3	
+
 
 if __name__ == "__main__":		#the main part
 
@@ -18,12 +19,18 @@ if __name__ == "__main__":		#the main part
 		(ans,prob) = rw.randomWalk1D(number_of_walks,point,[0,length],step_size)  #length starts at 0
 		#print(ans)
 		P_a.append(prob)		#Probability of reaching boundary(whilst Random Walking) for all the points
-		F.append(f*sum([len(i) for i in ans])*(step_size**2)*10e-4) #count number of points in each call to the randomWalk1D function
+		F.append(f*sum([len(i) for i in ans])*(step_size**2)*10e-4/number_of_walks) #count number of points in each call to the randomWalk1D function
 		#print(F)
 
 	print(P_a)
 	voltage_final = [(i*boundary_voltages[1]+ (1-i)*boundary_voltages[0])-j for i,j in zip(P_a,F)]  #calculated voltages at all points using Monte Carlo methods
 	voltage_actual = [boundary_voltages[0]+(i/length)*(boundary_voltages[1]- boundary_voltages[0])-(i**2)*(0.5*rho/epsilon_o)*10e-4 for i in points]   #actual distribution of voltages across the points
-	plt.plot(voltage_final, marker='o')
-	plt.plot(voltage_actual)
+
+	p1 = plt.plot(voltage_final, marker='o', markersize=4)
+	p2 = plt.plot(voltage_actual)
+	plt.title("POISSON EQUATION")
+	plt.xlabel("Distance from the low voltage capacitor plate (in cm)")
+	plt.ylabel("Voltage (in Volts)")
+	plt.xticks(range(0,length+step_size,2*step_size))
+	plt.legend((p1[0], p2[0]),('Calculated Voltage', 'Actual Voltage'))
 	plt.show()
