73a4dd37141cd3fc3e33e6b8e48095700e6896de
Vaibhav <vaiagr2011@gmail.com>
2020-05-30 01:54:22 +0530

    Minor comment changes

--- random_walk.py
+++ random_walk.py
@@ -1,9 +1,10 @@
-import random
-#import numpy as np
-import matplotlib.pyplot as plt
+import random                      # for random number generating functions
+#import numpy as np 
+import matplotlib.pyplot as py      #graph plotting library of python
 
-def randomWalk1D(number_of_walks, starting_point, boundary_condition, step_size):
-	path = []
+#function for a random walk in 1D with given parameters.
+def randomWalk1D(number_of_walks, starting_point, boundary_condition, step_size): 
+	path = []																	  
 	probability = 0
 
 	for i in range(number_of_walks):
@@ -11,21 +12,21 @@ def randomWalk1D(number_of_walks, starting_point, boundary_condition, step_size)
 		curr_point = starting_point
 		curr_path = []
 		while(True):
-			d = random.randint(0,1)
+			d = random.randint(0,1)     #generates random integers between 0 and 1
 			if curr_point in boundary_condition:
 				probability = probability+ (curr_point/boundary_condition[1])
 				#print(probability)
 				break 
-			if d==1:
+			if d==1:                #if d=1, move forward
 				curr_point = min(curr_point+step_size, boundary_condition[1])
-			else:
+			else:                   #if d=0, move backward
 				curr_point = max(curr_point-step_size, boundary_condition[0])
-			curr_path.append(curr_point)
-		path.append(curr_path)
+			curr_path.append(curr_point)    #stores one random walk
+		path.append(curr_path)      #stores the complete path for all random walks
 
 		
-	probability = probability/number_of_walks	
-	return (path,probability)
+	probability = probability/number_of_walks	#probability of reaching the right boundary
+	return (path,probability)                   #returns the path array and the probability array
 
 
 
